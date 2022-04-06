0b921aef067a7b3d3d9e62d9efeb25d8204f937d
Yonas Alemayehu <yonialemye@gmail.com>
2022-04-06 18:56:24 +0300

    setting up google map

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/home-page';
@@ -9,8 +12,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/app/utils/keys.dart
@@ -0,0 +1,1 @@
+const String mapApiKey = 'AIzaSyAR7GYZvunR5OOiuGjPJoU3yt1Zq5F18cw';
