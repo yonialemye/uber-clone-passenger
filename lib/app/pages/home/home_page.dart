5f02beb4ff8d21f303222ca41ea0e7124ff8ef5e
Yonas Alemayehu <yonialemye@gmail.com>
2022-04-07 21:37:09 +0300

    no changes

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -131,6 +131,21 @@ class _HomePageState extends State<HomePage> {
               }
               setState(() => bottomPadding = 365);
               getCurrentPosition();
+
+              scaffoldKey.currentState!.showBottomSheet(
+                (context) {
+                  return SearchFieldBottomSheet(
+                    searchController: searchController,
+                  );
+                },
+                enableDrag: false,
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(Values.radius30),
+                  ),
+                ),
+              );
             },
           ),
           Positioned(
@@ -149,102 +164,196 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).scaffoldBackgroundColor,
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(Values.radius30),
-                    ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: Values.height10),
-                      Align(
-                        alignment: Alignment.topCenter,
-                        child: Container(
-                          height: 5,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).primaryColor,
-                            borderRadius: BorderRadius.circular(
-                              Values.radius30,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: Values.height10),
-                      MyText(
-                        text: 'Welcome back!',
-                        fontSize: 11,
-                        fontFamily: 'Pop-Regular',
-                        textColor: Theme.of(context).textTheme.bodyText2!.color,
-                      ),
-                      SizedBox(height: Values.height10),
-                      MyText(
-                        text: 'Where do you want to go?',
-                        fontSize: 16,
-                        textColor: Theme.of(context).textTheme.bodyText2!.color,
-                      ),
-                      SizedBox(height: Values.height20),
-                      MyTextField(
-                        controller: searchController,
-                        labelText: 'Select location from map',
-                        prefixIcon: Icons.location_on_outlined,
-                        readOnly: true,
-                        keyBoardType: TextInputType.text,
-                        hintText: 'Hey',
-                      ),
-                      SizedBox(height: Values.height20),
-                      Hero(
-                        tag: ButtonsHero.elevated,
-                        child: MyElevatedButton(
-                          child: const Text('Calculate distance'),
-                          onPressed: () {},
-                        ),
-                      ),
-                      SizedBox(height: Values.height20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          MyText(
-                            text: 'Favorite Places',
-                            textColor: Theme.of(context).textTheme.bodyText2!.color,
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.add),
-                          ),
-                        ],
-                      ),
-                      Card(
-                        elevation: 4,
-                        color: Theme.of(context).scaffoldBackgroundColor,
-                        child: const ListTile(
-                          title: Text('Home'),
-                          subtitle: Text('Gondar Arada'),
-                          trailing: Icon(Icons.edit),
-                        ),
-                      ),
-                      SizedBox(height: Values.height20),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
+          // Positioned(
+          //   bottom: 0,
+          //   left: 0,
+          //   right: 0,
+          //   child: Column(
+          //     crossAxisAlignment: CrossAxisAlignment.stretch,
+          //     children: [
+          //       Container(
+          //         padding: const EdgeInsets.symmetric(horizontal: 20),
+          //         decoration: BoxDecoration(
+          //           color: Theme.of(context).scaffoldBackgroundColor,
+          //           borderRadius: BorderRadius.vertical(
+          //             top: Radius.circular(Values.radius30),
+          //           ),
+          //         ),
+          //         child: Column(
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //             SizedBox(height: Values.height10),
+          //             Align(
+          //               alignment: Alignment.topCenter,
+          //               child: Container(
+          //                 height: 5,
+          //                 width: 40,
+          //                 decoration: BoxDecoration(
+          //                   color: Theme.of(context).primaryColor,
+          //                   borderRadius: BorderRadius.circular(
+          //                     Values.radius30,
+          //                   ),
+          //                 ),
+          //               ),
+          //             ),
+          //             SizedBox(height: Values.height10),
+          //             MyText(
+          //               text: 'Welcome back!',
+          //               fontSize: 11,
+          //               fontFamily: 'Pop-Regular',
+          //               textColor: Theme.of(context).textTheme.bodyText2!.color,
+          //             ),
+          //             SizedBox(height: Values.height10),
+          //             MyText(
+          //               text: 'Where do you want to go?',
+          //               fontSize: 16,
+          //               textColor: Theme.of(context).textTheme.bodyText2!.color,
+          //             ),
+          //             SizedBox(height: Values.height20),
+          //             MyTextField(
+          //               controller: searchController,
+          //               labelText: 'Select location from map',
+          //               prefixIcon: Icons.location_on_outlined,
+          //               readOnly: true,
+          //               keyBoardType: TextInputType.text,
+          //               hintText: 'Hey',
+          //             ),
+          //             SizedBox(height: Values.height20),
+          //             Hero(
+          //               tag: ButtonsHero.elevated,
+          //               child: MyElevatedButton(
+          //                 child: const Text('Calculate distance'),
+          //                 onPressed: () {},
+          //               ),
+          //             ),
+          //             SizedBox(height: Values.height20),
+          //             Row(
+          //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //               children: [
+          //                 MyText(
+          //                   text: 'Favorite Places',
+          //                   textColor: Theme.of(context).textTheme.bodyText2!.color,
+          //                 ),
+          //                 IconButton(
+          //                   onPressed: () {},
+          //                   icon: const Icon(Icons.add),
+          //                 ),
+          //               ],
+          //             ),
+          //             Card(
+          //               elevation: 4,
+          //               color: Theme.of(context).scaffoldBackgroundColor,
+          //               child: const ListTile(
+          //                 title: Text('Home'),
+          //                 subtitle: Text('Gondar Arada'),
+          //                 trailing: Icon(Icons.edit),
+          //               ),
+          //             ),
+          //             SizedBox(height: Values.height20),
+          //           ],
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
         ],
       ),
     );
   }
 
   openDrawer() => scaffoldKey.currentState!.openDrawer();
+}
+
+class SearchFieldBottomSheet extends StatelessWidget {
+  const SearchFieldBottomSheet({Key? key, required this.searchController}) : super(key: key);
+
+  final TextEditingController searchController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(Values.radius30),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: Values.height10),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: 5,
+              width: 40,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(
+                  Values.radius30,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: Values.height10),
+          MyText(
+            text: 'Welcome back!',
+            fontSize: 11,
+            fontFamily: 'Pop-Regular',
+            textColor: Theme.of(context).textTheme.bodyText2!.color,
+          ),
+          SizedBox(height: Values.height10),
+          MyText(
+            text: 'Where do you want to go?',
+            fontSize: 16,
+            textColor: Theme.of(context).textTheme.bodyText2!.color,
+          ),
+          SizedBox(height: Values.height20),
+          MyTextField(
+            controller: searchController,
+            labelText: 'Select location from map',
+            prefixIcon: Icons.location_on_outlined,
+            readOnly: true,
+            keyBoardType: TextInputType.text,
+            hintText: 'Hey',
+          ),
+          SizedBox(height: Values.height20),
+          Hero(
+            tag: ButtonsHero.elevated,
+            child: MyElevatedButton(
+              child: const Text('Calculate distance'),
+              onPressed: () {},
+            ),
+          ),
+          SizedBox(height: Values.height20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              MyText(
+                text: 'Favorite Places',
+                textColor: Theme.of(context).textTheme.bodyText2!.color,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          Card(
+            elevation: 4,
+            color: Theme.of(context).scaffoldBackgroundColor,
+            child: const ListTile(
+              title: Text('Home'),
+              subtitle: Text('Gondar Arada'),
+              trailing: Icon(Icons.edit),
+            ),
+          ),
+          SizedBox(height: Values.height20),
+        ],
+      ),
+    );
+  }
 }
